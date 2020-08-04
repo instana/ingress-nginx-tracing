ece3285a283b5fc8a7cb709ea9851eb189866c1b
Sebastian Parschauer <sebastian.parschauer@instana.com>
2020-08-04 14:10:46 +0200

    Implement an automated build
    Add the ingress-nginx versions "nginx-0.25.1-rancher1" and
    "nginx-0.30.0" known to be used by customers to a configuration
    file. Add a build script and a Makefile. Add further scripts for
    config parsing, to determine the right NGINX tracing binary suffix,
    and drop the get_nginx_template.sh script as the template can be
    downloaded from GitHub directly.
    Furthermore, adapt the Dockerfile to be able to build OpenResty
    glibc based images. Rename LIBC_VERSION to LIBC_FLAVOR to be clear.
    Also add a first Jenkinsfile with a checkout and a build step.
    
    Build all:
    make
    
    Build only one config:
    make CONFIG_ENTRY=nginx-0.25.1-rancher1

---
+++ build_init_containers.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+set -e
+
+CWD="$(dirname $0)"
+cd "${CWD}"
+
+source "scripts/ini-parsing.sh"
+
+TMPL_PATH="rootfs/etc/nginx/template/nginx.tmpl"
+
+get_and_patch_nginx_template() {
+    rm -f init-container/nginx.tmpl
+    local tmpl_uri=$(sed 's/github.com/raw.githubusercontent.com/g' <<< ${repo})
+    tmpl_uri="${tmpl_uri}/${tag}/${TMPL_PATH}"
+    wget -O init-container/nginx.tmpl "${tmpl_uri}"
+    init-container/patch_nginx_template.sh
+}
+
+get_nginx_flavor_and_version() {
+    if [ -z "${nginx_version}" ]; then
+        nginx_version=$(docker run -t ${docker_image} \
+          nginx -v | cut -d ' ' -f3 | tr -d [:cntrl:])
+        echo "new nginx_version: ${nginx_version}"; echo
+    fi
+    nginx_flavor=$(cut -d '/' -f1 <<< ${nginx_version})
+    nginx_version=$(cut -d '/' -f2 <<< ${nginx_version})
+}
+
+dump_config() {
+    echo "Using config ${tag}:"
+    echo "repo: ${repo}"
+    echo "tag: ${tag}"
+    echo "docker_image: ${docker_image}"
+    echo "libc_flavor: ${libc_flavor}"
+    echo "nginx_version: ${nginx_version}"
+    echo
+}
+
+init_and_load_cfg() {
+    repo=""
+    tag=""
+    docker_image=""
+    libc_flavor="musl"
+    nginx_version=""
+    ${cfg}
+}
+
+build_init_container_image() {
+    nginx_flavor=""
+    init_and_load_cfg
+    dump_config
+    get_nginx_flavor_and_version
+    get_and_patch_nginx_template
+
+    # Build the init container image
+    docker build -t instana-nginx-init:${tag} ./init-container \
+        --build-arg libc_flavor="${libc_flavor}" \
+        --build-arg nginx_flavor="${nginx_flavor}" \
+        --build-arg nginx_version="${nginx_version}" \
+        --build-arg download_key="${INSTANA_DOWNLOAD_KEY}"
+}
+
+main() {
+    _parse_cfg init-container-config
+
+    # Only build from specified config entry
+    local selected_cfg="$1"
+    if [ -n "${selected_cfg}" ]; then
+        CFG_FUNCTIONS=("cfg.section.${selected_cfg}")
+    fi
+
+    for cfg in ${CFG_FUNCTIONS[*]}; do
+        build_init_container_image
+    done
+}
+
+main "$@"
