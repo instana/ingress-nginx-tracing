bfac2f18d702a11ee0a8a494a4ef4069884f25dd
Sebastian Parschauer <sebastian.parschauer@instana.com>
2020-08-12 14:17:50 +0200

    Upload to instana/release directly

--- build/upload_init_containers.sh
+++ build/upload_init_containers.sh
@@ -10,7 +10,7 @@ force_success() {
 }
 
 set_target_name() {
-    TARGET_NAME="${DOCKER_REGISTRY_INTERNAL}/instana/prerelease/ingress-nginx-init"
+    TARGET_NAME="${DOCKER_REGISTRY_INTERNAL}/instana/release/ingress-nginx-init"
 }
 
 clean_up() {
