07d78bee2c009a286330e04ff9c073d1f00a24de
Sebastian Parschauer <sebastian.parschauer@instana.com>
2020-08-14 13:27:31 +0200

    Build nginx-0.25.1-rancher1 binaries with compat
    The image in nginx-0.25.1-rancher1 uses OpenResty 1.15.8.1 which is
    built with compatibility. This is unusual for this version of
    OpenResty. So pass the tag name to get_suffix.sh and set the proper
    suffix.

--- build/init-container/get_suffix.sh
+++ build/init-container/get_suffix.sh
@@ -4,6 +4,7 @@ set -e
 
 NGINX_FLAVOR="$1"
 NGINX_VERSION="$2"
+TAG="$3"
 
 if [[ -z "${NGINX_FLAVOR}" || -z "${NGINX_VERSION}" ]]; then
   exit 0
@@ -12,6 +13,14 @@ fi
 if [ "${NGINX_FLAVOR}" != "openresty" ]; then
   exit 0
 fi
+
+case "${TAG}" in
+*0.25.1-rancher*)
+  # special: openresty 1.15.8.1 built with compat
+  echo -n "_compat"
+  exit 0
+  ;;
+esac
 
 case "${NGINX_VERSION}" in
 1.19.*|1.17.*|1.15.8.3)
