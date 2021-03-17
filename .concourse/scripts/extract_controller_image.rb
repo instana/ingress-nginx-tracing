a30c7f81808666ee97816bfdf908c5ee4637c0b8
Sebastian Parschauer <sebastian.parschauer@instana.com>
2021-03-17 17:36:56 +0100

    Make this repo obsolete.

--- .concourse/scripts/extract_controller_image.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'yaml'
-
-deployment_document = YAML.load_stream(ARGF.read).find { |document| document['kind'] == 'Deployment' && document['metadata']['labels'] && document['metadata']['labels']['app.kubernetes.io/component'] == 'controller' } 
-controller_container_spec = deployment_document['spec']['template']['spec']['containers'].find { |container| container['name'] == 'controller' }
-
-puts controller_container_spec['image']
